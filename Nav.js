28aecb1d95c7abafbbd9a55d6dd2b15cdddc77fb
anumthapa <anum.roy123@gmail.com>
2020-05-01 21:33:17 +0300

    1 file modified

--- Nav.js
+++ Nav.js
@@ -9,8 +9,8 @@ const Nav =()=>{
   <h2 className="mt2 mb0 f6 fw4 ttu tracked">Lets Start shopping</h2>
   <nav className="bt bb tc mw7 center mt4">
     <a className="f6 f5-l link bg-animate black-80 hover-bg-lightest-blue dib pa3 ph4-l" href="/">Home</a>
-    
-    <a className="f6 f5-l link bg-animate black-80 hover-bg-light-yellow dib pa3 ph4-l" href="/contact">Contact</a>
+    <a className="f6 f5-l link bg-animate black-80 hover-bg-light-yellow dib pa3 ph4-l" href="#">Contact</a>
+    <a className="f6 f5-l link bg-animate black-80 hover-bg-light-yellow dib pa3 ph4-l" href="/Cart">Cart</a>
   </nav>
 </header>      
      
