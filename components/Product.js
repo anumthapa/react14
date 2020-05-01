b97f56fd7773fa757bb0f5b247395884253caf4c
anumthapa <anum.roy123@gmail.com>
2020-05-01 21:32:02 +0300

    1 file modified

--- components/Product.js
+++ components/Product.js
@@ -4,7 +4,7 @@ const Product =({id, name, description,img, price})=>{
   return(
     <article class="mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10" id={id}>
   <div class="tc">
-    <img src={img} class="br-100 h4 w4 dib ba b--black-05 pa2" title="Photo of a kitty staring at you"/>
+    <img src={img} class="br-100 h4 w4 dib ba b--black-05 pa2" title="Cloths"/>
     <h1 className="f3 mb2">{name}</h1>
     <h2 className="f5 fw4 gray mt0">{description}</h2>
     <span> $ {price}</span><br/>
